325c1fc2f26248c15c0eb014ce2b8291dae5c7d9
Deji287 <slimdejavu0101@gmail.com>
2022-07-01 00:07:12 +0100

    Added a script that prints Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * A code that prints "Programming..." and a newline.
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
