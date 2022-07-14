674db36494c154a69dae3fd2701a0ffec3480873
Deji287 <slimdejavu0101@gmail.com>
2022-07-14 18:10:59 +0100

    C program for pointers,arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -36,7 +36,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	r[k] = '\0';
 	if (n1Size >= 0 || n2Size >= 0 || m)
-	return (0);
+		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
 		m = r[k];
