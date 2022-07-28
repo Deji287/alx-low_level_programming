6e9fdf8026e45a4875c52ce8940d4b924b96f6a8
Deji287 <slimdejavu0101@gmail.com>
2022-07-28 09:51:25 -0700

    C function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* string_nconcat - concatenates two strings.
-* @s1: first string to copy
-* @s2: second string to copy
-* @n: number of bytes of s2 to copy
-*
-* Return: char pointer to newly allocated place in memory
-*/
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
+ *
+ * Return: char pointer to newly allocated place in memory
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int length = n, i;
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		newstr[length++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
-		newstr[length++] = s2[i]
+		newstr[length++] = s2[i];
 
 	newstr[length] = '\0';
 
