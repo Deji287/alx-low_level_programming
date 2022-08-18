41b048d4c12a2e4f7537a83316f4092085fc181d
Deji287 <slimdejavu0101@gmail.com>
2022-08-18 04:24:30 -0700

    C function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,28 +1,16 @@
 #include "main.h"
-
 /**
- * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
- *
- * Return: void
- */
-void print_binary(unsigned long int n)
+* get_bit - returns the value of a bit at an index in a decimal number
+* @n: number to search
+* @index: index of the bit
+*
+* Return: value of the bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
 {
-	int i, num = 0;
-	unsigned long int temp;
-
-	for (i = 63; i >= 0; i--)
-	{
-		temp = n >> i;
-
-		if (temp & 1)
-		{
-			_putchar('1');
-			num++;
-		}
-		else if (num)
-			_putchar('0');
-	}
-	if (!num)
-		_putchar('0');
+int bit_val;
+if (index > 63)
+return (-1);
+bit_val = (n >> index) & 1;
+return (bit_val);
 }
